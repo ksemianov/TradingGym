395f9840b8d3df160e740ed825fac113cc248246
Konstantin Semianov <semyanovk@gmail.com>
2018-05-06 12:36:44 +0300

    Add proper init

--- OrderBook.py
+++ OrderBook.py
@@ -2,8 +2,21 @@ class OrderBook:
     """
     Implements data structure to append messages one at a time
     """
-    # bids, asks
-    book = (dict(), dict())
+    def __init__(self):
+        # bids, asks
+        self.book = (dict(), dict())
+    
+    def bestBid(self):
+        if not self.book[0].keys():
+            return (float('nan'), float('nan'))
+        price = max(self.book[0].keys())
+        return (price, self.book[0][price])
+    
+    def bestAsk(self):
+        if not self.book[1].keys():
+            return (float('nan'), float('nan'))
+        price = min(self.book[1].keys())
+        return (price, self.book[1][price])
     
     def update(self, message):
         buySell = 'Buy' in message.Flags
@@ -21,4 +34,8 @@ class OrderBook:
                 raise RuntimeError('Negative ammount is generated in order book')
             if self.book[1-buySell][price] == 0:
                 del self.book[1-buySell][price]
+                
+    def updateBulk(self, messages):
+        for name, message in messages.iterrows():
+            self.update(message)
                 
